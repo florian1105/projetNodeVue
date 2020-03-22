9b31d68a22f93928f3df22ba007a7b7ed9ac02bc
florian1105 <florian110599@gmail.com>
2020-03-22 21:33:24 +0100

    new user

--- index.js
+++ index.js
@@ -9,6 +9,8 @@ const cors = require('cors');
 const secret = 'thisismysecret'
 const jSonParser = bodyParser.json()
 const app = express()
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
 app.use(cors())
 
 const ExtractJwt = passportJWT.ExtractJwt
@@ -134,8 +136,10 @@ app.post('/login', jSonParser, async (req, res) => {
 
   // usually this would be a database call:
   var user = await ax.get(`/members?q={"email":"${email}"}`);
-  user=user.data[0];
-  if (user.password !== password) {
+  user=user.data[0]
+  if (bcrypt.compare(password, user.password, function(err, result) {
+    return result
+  })){
     res.status(401).json({ error: 'Email / password do not match.' })
     return
   }
@@ -144,6 +148,22 @@ app.post('/login', jSonParser, async (req, res) => {
 
   res.json({ jwt: userJwt })
 })
+
+app.post('/register',jSonParser , async (req,res) =>{
+  const member = req.body
+  bcrypt.hash(member.password, saltRounds, function(err, hash) {
+    const encMember = {
+      'username':member.username,
+      'email':member.email,
+      'password':hash
+    };
+    ax.post('/members',encMember).then(function (response) {
+      // handle success
+      res.json(response.data);
+    });
+  });
+
+});
 
 
 app.listen(PORT, function () {
